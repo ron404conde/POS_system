b2fcf506c35fccca95ae7819cfa470adcd7cebc4
ron404conde <sollezarondel@gmail.com>
2021-04-13 17:38:23 +0800

    Updated (Product Inventory Fill-Up)

--- app/src/main/java/com/example/pos_system/ProductInventory_FillUp.java
+++ app/src/main/java/com/example/pos_system/ProductInventory_FillUp.java
@@ -1,14 +1,35 @@
 package com.example.pos_system;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class ProductInventory_FillUp extends AppCompatActivity {
+
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_inventory_fill_up);
+
+        // Header back button
+        setActionBar_InventoryFillUp();
+        
     }
+
+    private void setActionBar_InventoryFillUp() {
+        actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.header_InventoryFillUp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.back_button);
+            actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0099FF")));
+        }
+    }
+
+
 }
