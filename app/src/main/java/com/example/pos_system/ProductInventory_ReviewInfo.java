707d2fe0ce6bf9ff26af39c0d5d294d771681c21
ron404conde <sollezarondel@gmail.com>
2021-04-14 19:52:46 +0800

    Added new Activity (ProductInventory_ReviewInfo)

---
+++ app/src/main/java/com/example/pos_system/ProductInventory_ReviewInfo.java
@@ -0,0 +1,14 @@
+package com.example.pos_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductInventory_ReviewInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_inventory_reviewinfo);
+    }
+}
