aeb6d3f85a789d21b8cd0d8008ab993255ea07bc
ron404conde <sollezarondel@gmail.com>
2021-04-20 10:24:54 +0800

    Update Dashboard Menu Item

--- app/src/main/java/com/example/pos_system/Sell_POS_system.java
+++ app/src/main/java/com/example/pos_system/Sell_POS_system.java
@@ -1,7 +1,6 @@
 package com.example.pos_system;
 
 import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -9,22 +8,17 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
 
-public class Dashboard_POS_system extends AppCompatActivity {
+public class Sell_POS_system extends AppCompatActivity {
 
     ActionBar actionBar;
-    private NavigationView nvDrawer;
-    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard_pos_system);
+        setContentView(R.layout.activity_sell_pos_system);
 
         bottomNavigation_menuItemClick();
         setActionBar_DashBoard();
@@ -33,7 +27,7 @@ public class Dashboard_POS_system extends AppCompatActivity {
     private void setActionBar_DashBoard() {
         actionBar = getSupportActionBar();
         if (actionBar != null) {
-            actionBar.setTitle(R.string.header_dashboard_pos);
+            actionBar.setTitle(R.string.header_sell_pos);
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.menu_icon);
             actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0099FF")));
@@ -43,24 +37,17 @@ public class Dashboard_POS_system extends AppCompatActivity {
     @SuppressLint("NonConstantResourceId")
     private void bottomNavigation_menuItemClick() {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setSelectedItemId(R.id.sell);
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             switch (item.getItemId()) {
-                case R.id.sell:
-                    startActivity(new Intent(getApplicationContext(), Sell_POS_system.class));
+                case R.id.home:
+                    startActivity(new Intent(getApplicationContext(), Dashboard_POS_system.class));
                     overridePendingTransition(0,0);
                     return true;
-                case R.id.home:
+                case R.id.sell:
                     return true;
             }
             return false;
         });
     }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
 }
